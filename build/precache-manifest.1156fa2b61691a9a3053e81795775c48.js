b813bc29f57028701d8788970e13dbf62a6229e1
thiagooliveira <thiagooliveira@digix.com.br>
2020-02-05 22:25:55 -0400

    Criando projeto react do arara tour

---
+++ build/precache-manifest.1156fa2b61691a9a3053e81795775c48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1a2ca671cdf495e70934e8638379eea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f14de0e1d713e62af7c",
+    "url": "/static/css/main.07a718d4.chunk.css"
+  },
+  {
+    "revision": "b829692f51f58c98b1bd",
+    "url": "/static/js/2.d29b3332.chunk.js"
+  },
+  {
+    "revision": "8f14de0e1d713e62af7c",
+    "url": "/static/js/main.44872c8e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
